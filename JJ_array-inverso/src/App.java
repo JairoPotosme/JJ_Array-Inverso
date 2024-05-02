246d188cffa9f20baef22467a255c0e67822574a
JairoPotosme <168260044+JairoPotosme@users.noreply.github.com>
2024-05-01 20:11:04 -0600

    Add files via upload

---
+++ JJ_array-inverso/src/App.java
@@ -0,0 +1,84 @@
+
+import java.util.Scanner;
+
+public class App {
+
+    public static int tamv = 0;
+    public static Scanner leer = new Scanner(System.in);
+
+    public static void main(String[] args) throws Exception {
+
+        boolean condiciosalida = true;
+
+        do {
+
+            try {
+
+                System.out.println(
+                        "Ingrese el tamaño del arreglo que no supere los 16 y sea mayor a 1 y que tampoco sea negativo ");
+                tamv = leer.nextInt();//10
+                condiciosalida = (tamv >= 2 && tamv <= 16) ? false : true;
+
+            } catch (Exception e ) {
+                System.out.println(e);
+                System.out.println("Ocurrio algo!. Revise por favor");
+                leer.nextLine();
+            }
+
+        } while (condiciosalida);
+
+        //recibimos el array del metodo
+        int array[] = llenado();
+
+        System.out.println("Mostrar");
+
+        Mostrar(array);
+
+    }
+
+    public static int[] llenado() {
+
+        int[] arreglo = new int[tamv];
+
+        for (int i = 0; i < arreglo.length; i++) {
+            System.out.println("ingrese el valor [" + i + "]");
+            arreglo[i] = leer.nextInt();
+        }
+
+        return arreglo;
+    }
+
+    public static void Mostrar(int[] a) {
+
+        for (int i : a) {
+            System.out.println(i);
+        }
+
+        
+
+    }
+    //Invertimos el arreglo
+
+    int auxiliar [] = new int [array.length];
+
+    for(int i = arreglo.length - 1,j = 0;i >= 0;i--,j++ ) {
+        auxiliar [j] =  array [i];
+
+    }
+    System.out.println(" El arreglo invertido es : ");
+    
+    for(int i = 0;i < auxiliar.length; i++){
+        System.out.println(auxiliar[i]);
+    }
+
+   
+   
+   
+   
+   
+   
+   
+    //Aqui va el metodo de inverso
+    //los voy a apalzar a todos
+
+}
